36a30482ba939e75c0c0a9a6ff86de30eb0141ba
NickBwalley <nickbwalley@gmail.com>
2023-10-10 00:52:54 +0300

    employee salary completed

--- resources/views/payroll/employeesalary.blade.php
+++ resources/views/payroll/employeesalary.blade.php
@@ -80,13 +80,12 @@
                         <table class="table table-striped custom-table datatable">
                             <thead>
                                 <tr>
-                                    <th>Employee</th>
+                                    <th>Employee Name</th>
                                     <th>Employee ID</th>
-                                    <th>Email</th>
-                                    <th>Join Date</th>
-                                    <th>Role</th>
-                                    <th>Salary</th>
-                                    <th>Payslip</th>
+                                    <th>KGS Harvested</th>
+                                    <th>Shilling per KG</th>
+                                    <th>Total Payout</th>
+                                    <th>Time</th>
                                     <th class="text-right">Action</th>
                                 </tr>
                             </thead>
@@ -132,6 +131,33 @@
                                 </tr>
                                 @endforeach
                             </tbody> --}}
+                            <tbody>
+                                @foreach ($users as $items)
+                                <tr>
+                                    <td>
+                                        <h2 class="table-avatar">
+                                            <a href="{{ url('employee/profile/'.$items->user_id) }}" class="avatar"><img alt="" src="{{ URL::to('/assets/images/'. $items->avatar) }}"></a>
+                                            <a href="{{ url('employee/profile/'.$items->user_id) }}">{{ $items->name }}</a>
+                                        </h2>
+                                    </td>
+                                    <td>{{ $items->employee_id_auto }}</td>
+                                    <td>{{ $items->number_of_kgs_harvested }}</td>
+                                    <td>{{ $items->shillings_per_kg }}</td>
+                                    <td>{{ $items->estimated_payout }}</td>
+                                    <td>{{ $items->created_at }}</td>
+                                    <td class="text-right">
+                                        <div class="dropdown dropdown-action">
+                                            <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item userSalary" href="#" data-toggle="modal" data-target="#edit_salary"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                <a class="dropdown-item salaryDelete" href="#" data-toggle="modal" data-target="#delete_salary"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
                         </table>
                     </div>
                 </div>
